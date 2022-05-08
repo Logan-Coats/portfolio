01b8784161d0a6dc8a199c9f1098b21d8957c0f2
Logan Coats <logancoats222@gmail.com>
2022-05-08 15:37:23 -0500

    brought in react example project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Ayo wuddup
+        </p>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          here is google
+        </a>
+        <p>
+          :)
+
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
